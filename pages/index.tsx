bd27f8c4615f6b1ee20d9664325ed6d2c2e3cfa2
ttt1091 <tez1091@yahoo.co.jp>
2021-10-15 19:12:03 +0900

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,6 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
